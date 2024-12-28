9da3d6f0382c3d8271ea3650fadb2cf609a1fa8a
Muhammed Bsoul <muhambs@ac.sce.ac.il>
2024-12-28 15:55:05 +0200

    edit books

---
+++ eBookLibraryService/Models/OwnedBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace eBookLibraryService.Models
+{
+    public class OwnedBook
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserEmail { get; set; } 
+        public Users User { get; set; }       
+
+        [Required]
+        public int BookId { get; set; }       
+        public Book Book { get; set; }       
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public bool IsBorrowed { get; set; }
+
+        [Required]
+        public float Price { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+        public DateTime? BorrowEndDate { get; set; } // Nullable for non-borrowed books
+    }
+}
